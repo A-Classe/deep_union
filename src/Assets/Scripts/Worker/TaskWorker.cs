9f4116f5835070efeeafafb2ae2d45bb227202f5
HarumaroJP <haruto09.game@gmail.com>
2023-08-16 20:02:40 +0900

    :fire: 開始時エージェント無効化機能の削除

--- src/Assets/Scripts/Worker/TaskWorker.cs
+++ src/Assets/Scripts/Worker/TaskWorker.cs
@@ -9,28 +9,18 @@ namespace Worker
     [RequireComponent(typeof(NavMeshAgent))]
     public class TaskWorker : MonoBehaviour
     {
-        [SerializeField] private bool enableAgentOnStart;
         private NavMeshAgent navMeshAgent;
         private Vector3 offset;
 
         public void OnSpawn(Vector3 spawnPoint)
         {
             navMeshAgent = GetComponent<NavMeshAgent>();
-            navMeshAgent.enabled = enableAgentOnStart;
-
             offset = transform.position - spawnPoint;
         }
 
         public void SetFollowPoint(Vector3 point)
         {
-            if (enableAgentOnStart)
-            {
-                navMeshAgent.SetDestination(point);
-            }
-            else
-            {
-                transform.position = Vector3.Lerp(transform.position, point + offset, Time.deltaTime * 2f);
-            }
+            navMeshAgent.SetDestination(point + offset);
         }
     }
 }
