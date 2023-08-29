f598b941048076138800d12ca0c034bfa9a0e3a4
HarumaroJP <haruto09.game@gmail.com>
2023-08-29 18:26:14 +0900

    :sparkles: ワーカーのモデルの組み込み

--- src/Assets/Scripts/Module/Working/State/FollowState.cs
+++ src/Assets/Scripts/Module/Working/State/FollowState.cs
@@ -7,18 +7,54 @@ namespace Module.Working.State
     {
         private readonly NavMeshAgent navMeshAgent;
         private readonly Worker worker;
+        private readonly Animator workerAnimator;
+        private static readonly int IsFollowing = Animator.StringToHash("Following");
 
         public FollowState(Worker worker)
         {
             this.worker = worker;
             navMeshAgent = worker.GetComponent<NavMeshAgent>();
+            workerAnimator = worker.animator;
         }
 
         public WorkerState WorkerState => WorkerState.Following;
 
+        private bool isMoving;
+
+        public void OnStart() { }
+
+        public void OnStop()
+        {
+            workerAnimator.SetBool(IsFollowing, false);
+        }
+
         public void Update()
         {
             navMeshAgent.SetDestination(worker.Target.position + worker.Offset);
+
+            if (IsStopped())
+            {
+                isMoving = false;
+                workerAnimator.SetBool(IsFollowing, false);
+            }
+            else if (IsMoved())
+            {
+                isMoving = true;
+                workerAnimator.SetBool(IsFollowing, true);
+            }
         }
+
+        bool IsStopped()
+        {
+            return isMoving && navMeshAgent.velocity.sqrMagnitude == 0f;
+        }
+
+
+        bool IsMoved()
+        {
+            return !isMoving && navMeshAgent.velocity.sqrMagnitude > 0f;
+        }
+
+        public void Dispose() { }
     }
 }
--- src/Assets/Scripts/Module/Working/State/WorkState.cs
+++ src/Assets/Scripts/Module/Working/State/WorkState.cs
@@ -1,24 +1,72 @@
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
 using UnityEngine.AI;
+using Wanna.DebugEx;
 
 namespace Module.Working.State
 {
     public class WorkState : IWorkerState
     {
         private readonly NavMeshAgent navMeshAgent;
-
         private readonly Worker worker;
+        private readonly Animator workerAnimator;
+
+        private static readonly int IsFollowing = Animator.StringToHash("Following");
+        private static readonly int IsWorking = Animator.StringToHash("Working");
+
+        private CancellationTokenSource cTokenSource;
 
         public WorkState(Worker worker)
         {
             this.worker = worker;
             navMeshAgent = worker.GetComponent<NavMeshAgent>();
+            workerAnimator = worker.animator;
         }
 
         public WorkerState WorkerState => WorkerState.Working;
 
+        public void OnStart()
+        {
+            cTokenSource?.Dispose();
+            cTokenSource = new CancellationTokenSource();
+
+            SequenceWork(cTokenSource.Token).Forget();
+        }
+
+        async UniTaskVoid SequenceWork(CancellationToken cancellationToken)
+        {
+            workerAnimator.SetBool(IsFollowing, true);
+
+            await UniTask.WaitUntil(IsArrived, cancellationToken: cancellationToken);
+
+            workerAnimator.SetBool(IsFollowing, false);
+            workerAnimator.SetBool(IsWorking, true);
+            navMeshAgent.obstacleAvoidanceType = ObstacleAvoidanceType.NoObstacleAvoidance;
+        }
+
+        private bool IsArrived()
+        {
+            return navMeshAgent != null && navMeshAgent.remainingDistance == 0f;
+        }
+
+        public void OnStop()
+        {
+            Dispose();
+            workerAnimator.SetBool(IsWorking, false);
+            navMeshAgent.obstacleAvoidanceType = ObstacleAvoidanceType.HighQualityObstacleAvoidance;
+        }
+
         public void Update()
         {
             navMeshAgent.SetDestination(worker.Target.position);
+        }
+
+        public void Dispose()
+        {
+            cTokenSource?.Cancel();
+            cTokenSource?.Dispose();
+            cTokenSource = null;
         }
     }
 }
