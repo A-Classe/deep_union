d07d1d8e747c05b1109e492a8cabe9396a2b8030
HarumaroJP <haruto09.game@gmail.com>
2023-09-23 11:04:29 +0900

    WIP

---
+++ src/Assets/Scripts/GameMain/Task/PoisonArea.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameMain.Task
+{
+    public class PoisonArea : MonoBehaviour
+    {
+        
+    }
+}
