e360ecefe3f9d15de3f2c63a421b42b5d7960530
HarumaroJP <haruto09.game@gmail.com>
2023-10-05 13:37:49 +0900

    :+1: カメラも追従して少し動くようにした

--- src/Assets/Scripts/Module/Player/Camera/CameraController.cs
+++ src/Assets/Scripts/Module/Player/Camera/CameraController.cs
@@ -7,6 +7,10 @@ namespace Module.Player.Camera
     [RequireComponent(typeof(UnityEngine.Camera))]
     public class CameraController : MonoBehaviour
     {
+        [SerializeField] private Transform leaderTarget;
+        [SerializeField] private float maxRotationRate = 0.3f;
+        [SerializeField] private float rotationSpeed = 1f;
+
         private ICameraState currentState;
         private ICameraState[] states;
 
@@ -14,10 +18,10 @@ namespace Module.Player.Camera
         private float depth = 14f;
         private double followAngle = 85f;
         private double cameraAngle = 65f;
+
         private void Awake()
         {
             Initialize();
-            
         }
 
         private void FixedUpdate()
@@ -25,21 +29,23 @@ namespace Module.Player.Camera
             if (followTarget == null) return;
             var angleInRadians = followAngle * Mathf.Deg2Rad;
 
-
             var y = depth * Math.Sin(angleInRadians);
             var horizontalDistance = depth * Math.Cos(angleInRadians);
 
-
             Vector3 horizontalOffset = -followTarget.forward * (float)horizontalDistance;
-
-
             Vector3 targetPosition = followTarget.position + horizontalOffset + Vector3.up * (float)y;
 
-       
             transform.position = targetPosition;
-
-         
             transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler((float)cameraAngle, 0f, 0f), Time.deltaTime);
+
+            RotateToTarget();
+        }
+
+        private void RotateToTarget()
+        {
+            var targetRotation = Quaternion.LookRotation(leaderTarget.position - transform.position);
+            var slerpRotation = Quaternion.Slerp(transform.rotation, targetRotation, maxRotationRate);
+            transform.rotation = Quaternion.Slerp(transform.rotation, slerpRotation, Time.fixedDeltaTime * rotationSpeed);
         }
 
         private void Initialize()
@@ -61,18 +67,15 @@ namespace Module.Player.Camera
             };
         }
 
-        public void InitParam()
-        {
-            
-        }
+        public void InitParam() { }
 
         public void SetFollowTarget(Transform player)
         {
             followTarget = player;
-            
+
             var position = transform.position;
             depth = Vector3.Distance(position, player.position);
-            
+
             var angle = Math.Atan2(position.y - player.position.y, position.z - player.position.z);
             followAngle = 180d - (angle * 180d / Math.PI);
 
