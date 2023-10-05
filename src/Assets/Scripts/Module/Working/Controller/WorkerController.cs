00c2f6f22479510b93f916ee269f29a4c6253e6a
HarumaroJP <haruto09.game@gmail.com>
2023-10-05 13:21:09 +0900

    :+1: ライトの移動に慣性を追加

--- src/Assets/Scripts/Module/Working/Controller/WorkerController.cs
+++ src/Assets/Scripts/Module/Working/Controller/WorkerController.cs
@@ -1,5 +1,7 @@
+using System;
 using Core.Input;
 using UnityEngine;
+using Wanna.DebugEx;
 
 namespace Module.Working.Controller
 {
@@ -10,10 +12,15 @@ namespace Module.Working.Controller
     {
         [Header("移動速度")] [SerializeField] private float controlSpeed;
 
+        [Header("静的摩擦力")] [SerializeField] private float staticFriction;
+        [Header("最大速度")] [SerializeField] private float maxSpeed;
+
+        [SerializeField] private Vector3 velocity;
+
         private InputEvent controlEvent;
 
         private Camera followCamera;
-        private Vector3 velocity;
+        private Vector2 input;
 
         private void Awake()
         {
@@ -23,37 +30,58 @@ namespace Module.Working.Controller
 
         private void Update()
         {
-            //リーダーの速度の更新
-            UpdateLeaderVelocity();
+            input = controlEvent.ReadValue<Vector2>();
         }
 
-        private void UpdateLeaderVelocity()
+        private void FixedUpdate()
         {
-            var input = controlEvent.ReadValue<Vector2>();
-
-            float moveX = input.x * controlSpeed * Time.deltaTime;
-            float moveY = input.y * controlSpeed * Time.deltaTime;
-
-            Vector3 move = new Vector3(moveX, 0f, moveY);
-
-            velocity += move;
-            if (!InViewport(transform.position)) return;
-
-            transform.position += velocity * Time.deltaTime;
-        }
-
-        private bool InViewport(Vector3 position)
-        {
-            if (followCamera != null)
+            if (input != Vector2.zero)
             {
-                Vector3 inViewport = followCamera.WorldToViewportPoint(position);
+                Vector2 vel = input * (controlSpeed * Time.fixedDeltaTime);
+                velocity += new Vector3(vel.x, 0, vel.y);
+            }
+            else
+            {
+                float friction = staticFriction * Time.fixedDeltaTime;
+                float frictionMagnitude = velocity.magnitude - friction;
 
-                return (inViewport.x is > 0 and < 1 &&
-                        inViewport.y is > 0 and < 1 &&
-                        inViewport.z > 0);
+                velocity = velocity.normalized * frictionMagnitude;
             }
 
-            return false;
+            velocity = Vector3.ClampMagnitude(velocity, maxSpeed);
+
+            Vector3 nextPos = transform.position + velocity * Time.fixedDeltaTime;
+
+            if (!InViewport(nextPos))
+            {
+                velocity = Vector3.zero;   
+            }
+
+            transform.position = Clamp(nextPos);
+        }
+
+        private bool InViewport(Vector3 worldPoint)
+        {
+            Vector3 inViewport = followCamera.WorldToViewportPoint(worldPoint);
+
+            return (inViewport.x is > 0 and < 1 &&
+                    inViewport.y is > 0 and < 1 &&
+                    inViewport.z > 0);
+        }
+
+
+        private Vector3 Clamp(Vector3 nextPosition)
+        {
+            Vector3 viewportPoint = followCamera.WorldToViewportPoint(nextPosition);
+
+            // オブジェクトの位置が画面外に出ないようにクランプ
+            viewportPoint.x = Mathf.Clamp01(viewportPoint.x);
+            viewportPoint.y = Mathf.Clamp01(viewportPoint.y);
+
+            Vector3 worldPoint = followCamera.ViewportToWorldPoint(viewportPoint);
+            worldPoint.y = nextPosition.y;
+
+            return worldPoint;
         }
 
         public void SetCamera(Camera cam)
