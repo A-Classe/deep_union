3e76d00daa19752a4b9a4ff7643716192d95d6a2
HarumaroJP <haruto09.game@gmail.com>
2023-10-09 22:38:54 +0900

    wip

--- src/Assets/Scripts/GameMain/Task/HealTask.cs
+++ src/Assets/Scripts/GameMain/Task/HealTask.cs
@@ -1,17 +1,22 @@
 using System;
 using Module.Assignment;
+using Module.Assignment.Component;
 using Module.Player.Controller;
 using Module.Task;
 using UnityEngine;
 using UnityEngine.AI;
 using VContainer;
+using Wanna.DebugEx;
 
 namespace GameMain.Task
 {
     public class HealTask : BaseTask
     {
         [SerializeField] private NavMeshAgent navMeshAgent;
+        [SerializeField] private AssignableArea assignableArea;
         [SerializeField] private uint addHp;
+        [SerializeField] private int minWorkerCount;
+        [SerializeField] private int maxWorkerCount;
 
         private PlayerController playerController;
         private PlayerStatus playerStatus;
@@ -20,18 +25,40 @@ namespace GameMain.Task
         {
             playerController = container.Resolve<PlayerController>();
             playerStatus = container.Resolve<PlayerStatus>();
+
+            ForceComplete();
+
+            assignableArea.OnWorkerEnter += _ =>
+            {
+                if (WorkerCount > maxWorkerCount)
+                    navMeshAgent.enabled = false;
+                else if (WorkerCount >= minWorkerCount)
+                    navMeshAgent.enabled = true;
+            };
+
+            assignableArea.OnWorkerExit += _ =>
+            {
+                if (WorkerCount < minWorkerCount)
+                    navMeshAgent.enabled = false;
+            };
+
+            navMeshAgent.updateRotation = false;
         }
 
         private void Update()
         {
-            navMeshAgent.SetDestination(playerController.transform.position);
+            if (navMeshAgent.enabled)
+            {
+                navMeshAgent.SetDestination(playerController.transform.position);
+            }
         }
 
-        private void OnTriggerEnter(Collider other)
+        private void OnCollisionEnter(Collision other)
         {
             if (other.gameObject.CompareTag("Player"))
             {
                 playerStatus.AddHp(addHp);
+                Disable();
             }
         }
     }
