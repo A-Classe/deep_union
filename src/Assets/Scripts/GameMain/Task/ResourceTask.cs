75f1dc5bdf7aafb52b65fc088c0d8609e1c27a5d
HarumaroJP <haruto09.game@gmail.com>
2023-10-21 15:09:50 +0900

    wip

--- src/Assets/Scripts/GameMain/Task/ResourceTask.cs
+++ src/Assets/Scripts/GameMain/Task/ResourceTask.cs
@@ -1,6 +1,11 @@
 using Core.NavMesh;
 using Cysharp.Threading.Tasks;
+using DG.Tweening;
+using Module.Assignment.Component;
+using Module.Player.Controller;
 using Module.Task;
+using Module.Working;
+using UnityEngine;
 using VContainer;
 
 namespace GameMain.Task
@@ -10,11 +15,22 @@ namespace GameMain.Task
     /// </summary>
     public class ResourceTask : BaseTask
     {
+        [SerializeField] private AssignableArea assignableArea;
+        [SerializeField] private CollectableTask collectableTask;
+        [SerializeField] private GameObject resourceItem;
         private RuntimeNavMeshBaker navMeshBaker;
+        private PlayerController playerController;
 
         public override void Initialize(IObjectResolver container)
         {
             navMeshBaker = container.Resolve<RuntimeNavMeshBaker>();
+            playerController = container.Resolve<PlayerController>();
+
+            collectableTask.OnCollected += count =>
+            {
+                Worker worker = assignableArea.AssignedWorkers[Random.Range(0, assignableArea.AssignedWorkers.Count)];
+                GameObject item = Instantiate(resourceItem, worker.transform.position, Quaternion.identity);
+            };
         }
 
         protected override void OnComplete()
