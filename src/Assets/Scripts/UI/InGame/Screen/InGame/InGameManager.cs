b9bad3a3064e7e3490f137d40a1af25db5c37bc5
ichiharayuuma <youma.20031209@gmail.com>
2023-10-29 11:48:01 +0900

    fix Handle move area & Slider color

--- src/Assets/Scripts/UI/InGame/Screen/InGame/InGameManager.cs
+++ src/Assets/Scripts/UI/InGame/Screen/InGame/InGameManager.cs
@@ -1,6 +1,8 @@
 using AnimationPro.RunTime;
+using Codice.Client.BaseCommands;
 using Core.Utility.UI;
 using Core.Utility.UI.Navigation;
+using System;
 using TMPro;
 using Unity.Plastic.Antlr3.Runtime;
 using UnityEngine;
@@ -15,6 +17,7 @@ namespace UI.InGame.Screen.InGame
         [SerializeField] private TextMeshProUGUI maxHp;
         [SerializeField] private Slider sliderHp;
         [SerializeField] private Image gaugeHpBackground;
+        [SerializeField] private Image HandleBackground;
         private uint? maxHpValue;
         
         // progress
@@ -25,6 +28,11 @@ namespace UI.InGame.Screen.InGame
         [SerializeField] private TextMeshProUGUI maxRes;
         [SerializeField] private Image gaugeRes;
         private uint? maxResValue;
+
+        // color
+        // Å‘åHP‚ÌF@x:Hue, y:Satulation, z:Value
+        private Vector3 maxHpHSV = new(109 / 360f, 1f, 1f);
+        private Vector3 zeroHpHSV = new(0f, 1f, 0.74f);
 
         private const float StartOffsetAmount = 0.37f;
         private const float EndOffsetAmount = 0.63f;
@@ -65,10 +73,20 @@ namespace UI.InGame.Screen.InGame
 
             currentHp.text = current.ToString();
             sliderHp.value = current;
-            gaugeHpBackground.color = current switch {
-                _ when current > maxHpValue.Value * 0.3f => Color.green,
-                _ => Color.red
+            //gaugeHpBackground.color = current switch {
+            //    _ when current > maxHpValue.Value * 0.3f => Color.green,
+            //    _ => Color.red
+            //};
+            var t = (float)current / (float)maxHpValue;
+            Func<Vector3, Vector3, float, Color> changeColor = (zeroHpHSV, maxHpHSV, t) =>
+            {
+                Vector3 lifeColorHSV = new(zeroHpHSV.x+((maxHpHSV.x - zeroHpHSV.x)*t), zeroHpHSV.y+((maxHpHSV.y - zeroHpHSV.y)*t), zeroHpHSV.z+((maxHpHSV.z - zeroHpHSV.z)*t));
+                Color color = Color.HSVToRGB(lifeColorHSV.x, lifeColorHSV.y, lifeColorHSV.z);
+                Debug.Log(color);
+                return color;
             };
+            gaugeHpBackground.color = changeColor(zeroHpHSV, maxHpHSV, t);
+            HandleBackground.color = changeColor(zeroHpHSV, maxHpHSV, t);
         }
 
         public void SetStageProgress(uint value)
