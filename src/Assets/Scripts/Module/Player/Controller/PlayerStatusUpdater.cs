ace45025103f19ae324b86c8cab8ebe2466a71b0
HarumaroJP <haruto09.game@gmail.com>
2023-10-31 17:34:06 +0900

    :+1: HPが定期的に減るように実装

--- src/Assets/Scripts/Module/Player/Controller/PlayerStatusUpdater.cs
+++ src/Assets/Scripts/Module/Player/Controller/PlayerStatusUpdater.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using GameMain.Presenter;
 using UI.InGame;
 using UnityEngine;
 using VContainer;
@@ -5,23 +9,30 @@ using VContainer.Unity;
 
 namespace Module.Player.Controller
 {
-    public class PlayerStatusUpdater : IInitializable
+    public class PlayerStatusUpdater : IInitializable, IDisposable
     {
         private readonly PlayerStatus playerStatus;
+        private readonly GameParam gameParam;
         private readonly PlayerStatusVisualizer statusVisualizer;
         private readonly InGameUIManager uiManager;
+
+        private CancellationTokenSource cTokenSource;
         private short maxHp;
 
         [Inject]
         public PlayerStatusUpdater(
-            PlayerStatus playerStatus, 
+            PlayerStatus playerStatus,
+            GameParam gameParam,
             PlayerStatusVisualizer statusVisualizer,
             InGameUIManager uiManager
         )
         {
             this.playerStatus = playerStatus;
+            this.gameParam = gameParam;
             this.statusVisualizer = statusVisualizer;
             this.uiManager = uiManager;
+
+            cTokenSource = new CancellationTokenSource();
         }
 
         public void Initialize()
@@ -34,6 +45,22 @@ namespace Module.Player.Controller
                 uiManager.SetHP(hp);
                 statusVisualizer.SetHpRate(1 - Mathf.InverseLerp(0f, maxHp, hp));
             };
+
+            DecreaseHpLoop(cTokenSource.Token).Forget();
+        }
+
+        async UniTaskVoid DecreaseHpLoop(CancellationToken cancellationToken)
+        {
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                await UniTask.Delay(TimeSpan.FromSeconds(gameParam.DecereseHpSpeed), cancellationToken: cancellationToken);
+                playerStatus.RemoveHp(gameParam.DecereseHpAmount);
+            }
+        }
+
+        public void Dispose()
+        {
+            cTokenSource?.Dispose();
         }
     }
 }
