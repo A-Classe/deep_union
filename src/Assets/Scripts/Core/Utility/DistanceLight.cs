a0f99b3aa1c86c2c6166a2f32c299464cc095728
HarumaroJP <haruto09.game@gmail.com>
2023-11-11 03:44:04 +0900

    オクルージョンカリングの適用

---
+++ src/Assets/Scripts/Core/Utility/DistanceLight.cs
@@ -0,0 +1,44 @@
+using System;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using Wanna.DebugEx;
+
+namespace Core.Utility
+{
+    public class DistanceLight : MonoBehaviour
+    {
+        [SerializeField] private Light myLight;
+        [SerializeField] [Range(0, 1)] private float updateInterval = 0.1f;
+        [SerializeField] private float allowDistance = 5f;
+
+        private Transform camTransform;
+        private bool isDisappeared;
+
+        private void Awake()
+        {
+            camTransform = Camera.main.transform;
+            AdjustLODQuality().Forget();
+        }
+
+        private async UniTaskVoid AdjustLODQuality()
+        {
+            while (!this.GetCancellationTokenOnDestroy().IsCancellationRequested)
+            {
+                float squareDistanceFromCamera = (camTransform.position - transform.position).sqrMagnitude;
+
+                if (squareDistanceFromCamera <= allowDistance)
+                {
+                    myLight.enabled = true;
+                    isDisappeared = false;
+                }
+                else if (!isDisappeared)
+                {
+                    myLight.enabled = false;
+                    isDisappeared = true;
+                }
+
+                await UniTask.Delay(TimeSpan.FromSeconds(updateInterval));
+            }
+        }
+    }
+}
