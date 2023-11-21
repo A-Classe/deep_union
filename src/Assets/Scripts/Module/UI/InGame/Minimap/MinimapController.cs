3a15ac354b8d7b8dd0da340d91c3e5d6339ada3f
yuhi yamane <bird9.yuhi@gmail.com>
2023-11-21 17:31:44 +0900

    focus修正

--- src/Assets/Scripts/Module/UI/InGame/Minimap/MinimapController.cs
+++ src/Assets/Scripts/Module/UI/InGame/Minimap/MinimapController.cs
@@ -16,34 +16,27 @@ namespace Module.UI.InGame.Minimap
         [SerializeField] private RectTransform rootUI;
         [SerializeField] private RectTransform mapRenderer;
         [SerializeField] private Transform player;
-        [SerializeField] private RectTransform center;
         private ControlState moveState;
         private MinimapState state;
         
         private Camera renderCamera;
 
-        private Dictionary<MinimapState, InStateProfile> profiles = new()
-        {
-            { MinimapState.Focus, new InStateProfile
-            {
-                RootPositionAnchor = new Vector3(0f, 0f, 0f),
-                RootUIScale = new Vector3(6f, 6f, 6f),
-                MapScale = new Vector3(1f, 1f, 1f),
-            } }
-        };
+        private Vector3 mapDefaultPos;
+
+        private Dictionary<MinimapState, InStateProfile> profiles = new();
 
 
         private void Start()
         {
             SetProfile(MinimapState.MiniView, new InStateProfile
             {
-                MapScale = mapRenderer.localScale,
+                MapScale = new Vector3(6f, 6f, 6f),
                 RootPositionAnchor = rootUI.anchoredPosition,
                 RootUIScale = rootUI.localScale
             });
             SetProfile(MinimapState.Focus, new InStateProfile
             {
-                MapScale = new Vector3(4f, 4f, 4f),
+                MapScale = new Vector3(3f, 3f, 3f),
                 RootPositionAnchor = Vector2.zero,
                 RootUIScale = new Vector3(4f, 4f, 4f)
             });
@@ -53,11 +46,30 @@ namespace Module.UI.InGame.Minimap
         public void First()
         {
             DisableFocusView();
+            // mapのbottomをrootのbottomに合わせる
+            var pos = rootUI.position;
+            pos.y -= (rootUI.rect.height / 2f) * rootUI.localScale.y;
+            pos.x = mapRenderer.position.x;
+            pos.y += (mapRenderer.rect.height / 2f) * mapRenderer.localScale.y;
+            mapRenderer.position = pos;
+
+
+            mapDefaultPos = pos;
         }
 
+        private Vector3 lastMovePosition = Vector3.zero;
         private void Update()
         {
-            
+            Vector3 pos = mapDefaultPos;
+            Vector3 viewPortValue = renderCamera.WorldToViewportPoint(player.transform.position);
+            pos.y -= viewPortValue.y * (mapRenderer.rect.height * mapRenderer.localScale.y);
+            Vector3 mapPos = mapRenderer.position;
+            mapPos.y = pos.y;
+            mapRenderer.position = mapPos;
+            lastMovePosition = player.transform.position;
+            if (Vector3.Distance(lastMovePosition, mapRenderer.position) > 1f)
+            {
+            }
         }
 
         internal void SetProfile(MinimapState key, InStateProfile profile)
@@ -85,12 +97,11 @@ namespace Module.UI.InGame.Minimap
             rootUI.anchoredPosition = profiles[state].RootPositionAnchor;
             rootUI.localScale = profiles[state].RootUIScale;
             mapRenderer.localScale = profiles[state].MapScale;
-
-            Vector3 pos = renderCamera.WorldToViewportPoint(player.position);
             
-            RectTransformUtility.ScreenPointToLocalPointInRectangle(mapRenderer, pos, null, out Vector2 localPoint);
-            Debug.Log(localPoint);
-            center.localPosition = localPoint;
+            // float referenceBottom = rootUI.anchoredPosition.y - rootUI.rect.height / 2;
+            //
+            // float targetY = referenceBottom + mapRenderer.rect.height / 2;
+            // mapRenderer.anchoredPosition = new Vector2(mapRenderer.anchoredPosition.x, targetY);
         }
 
         public void SetCamera(Vector3 pos)
