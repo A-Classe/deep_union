5fce1e4b3098d402891b9d74658b759c2d985a83
yuhi yamane <bird9.yuhi@gmail.com>
2023-11-21 18:10:20 +0900

    WIP

--- src/Assets/Scripts/Module/UI/InGame/Minimap/MinimapController.cs
+++ src/Assets/Scripts/Module/UI/InGame/Minimap/MinimapController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -55,20 +56,63 @@ namespace Module.UI.InGame.Minimap
 
 
             mapDefaultPos = pos;
+            moveState = ControlState.Idle;
         }
 
         private Vector3 lastMovePosition = Vector3.zero;
-        private void Update()
+        private void FixedUpdate()
         {
-            Vector3 pos = mapDefaultPos;
-            Vector3 viewPortValue = renderCamera.WorldToViewportPoint(player.transform.position);
-            pos.y -= viewPortValue.y * (mapRenderer.rect.height * mapRenderer.localScale.y);
-            Vector3 mapPos = mapRenderer.position;
-            mapPos.y = pos.y;
-            mapRenderer.position = mapPos;
-            lastMovePosition = player.transform.position;
+            switch (moveState)
+            {
+                case ControlState.MoveToFocus:
+                    UpdateInMoveFocus();
+                    break;
+                case ControlState.MoveToMiniView:
+                    UpdateInMoveMinimap();
+                    break;
+                case ControlState.Idle:
+                    UpdateInCamera();
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        private void UpdateInCamera()
+        {
             if (Vector3.Distance(lastMovePosition, mapRenderer.position) > 1f)
             {
+                Vector3 pos = mapDefaultPos;
+                Vector3 viewPortValue = renderCamera.WorldToViewportPoint(player.transform.position);
+                pos.y -= viewPortValue.y * (mapRenderer.rect.height * mapRenderer.localScale.y);
+                Vector3 mapPos = mapRenderer.position;
+                mapPos.y = pos.y;
+                mapRenderer.position = mapPos;
+                lastMovePosition = player.transform.position;
+            }
+        }
+
+        private void UpdateInMoveFocus()
+        {
+            rootUI.anchoredPosition = Vector2.Lerp(rootUI.anchoredPosition, profiles[state].RootPositionAnchor, Time.deltaTime);
+            rootUI.localScale = Vector3.Lerp(rootUI.localScale, profiles[state].RootUIScale, Time.deltaTime);
+           // mapRenderer.localScale = Vector3.Lerp(mapRenderer.localScale, profiles[state].MapScale, Time.deltaTime);
+            if (Vector2.Distance(rootUI.anchoredPosition, profiles[state].RootPositionAnchor) < 0.1f)
+            {
+                UpdateInProfile();
+                moveState = ControlState.Idle;
+            }
+        }
+
+        private void UpdateInMoveMinimap()
+        {
+            rootUI.anchoredPosition = Vector2.Lerp(rootUI.anchoredPosition, profiles[state].RootPositionAnchor, Time.deltaTime);
+            rootUI.localScale = Vector3.Lerp(rootUI.localScale, profiles[state].RootUIScale, Time.deltaTime);
+          //  mapRenderer.localScale = Vector3.Lerp(mapRenderer.localScale, profiles[state].MapScale, Time.deltaTime);
+            if (Vector2.Distance(rootUI.anchoredPosition, profiles[state].RootPositionAnchor) < 0.1f)
+            {
+                UpdateInProfile();
+                moveState = ControlState.Idle;
             }
         }
 
@@ -80,12 +124,14 @@ namespace Module.UI.InGame.Minimap
 
         public void EnableFocusView()
         {
+            moveState = ControlState.MoveToFocus;
             state = MinimapState.Focus;
             UpdateInProfile();
         }
 
         public void DisableFocusView()
         {
+            moveState = ControlState.MoveToMiniView;
             state = MinimapState.MiniView;
             UpdateInProfile();
         }
@@ -97,7 +143,6 @@ namespace Module.UI.InGame.Minimap
             rootUI.anchoredPosition = profiles[state].RootPositionAnchor;
             rootUI.localScale = profiles[state].RootUIScale;
             mapRenderer.localScale = profiles[state].MapScale;
-            
             // float referenceBottom = rootUI.anchoredPosition.y - rootUI.rect.height / 2;
             //
             // float targetY = referenceBottom + mapRenderer.rect.height / 2;
