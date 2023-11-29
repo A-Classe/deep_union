cf9ee467be1b9fedea6fbaa9d7538e544f8f6660
Ayane2003 <g2227056@st.t-kougei.ac.jp>
2023-11-29 20:53:45 +0900

    stageTutorialにマテリアル付与

--- src/Assets/Scripts/Tutorial/ImageTrigger.cs
+++ src/Assets/Scripts/Tutorial/ImageTrigger.cs
@@ -5,6 +5,8 @@ public class ImageTrigger : MonoBehaviour
 {
     public RawImage rawImage; // RawImageコンポーネントを使用する
     [SerializeField] private float Time = 3.0f;//デフォルトは3秒
+    [SerializeField] private string Tag = "Player";
+    [SerializeField] private GameObject HideObj = null;
 
     private void Start()
     {
@@ -13,23 +15,17 @@ public class ImageTrigger : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag(Tag))
         {
             rawImage.enabled = true; // 画像を表示する
             Invoke("HideImage", Time); // Time秒後にHideImageメソッドを呼ぶ
         }
     }
 
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            HideImage(); // プレイヤーがエリアから出たらすぐに非表示にする
-        }
-    }
 
     private void HideImage()
     {
         rawImage.enabled = false; // 画像を非表示にする
+        HideObj.SetActive(false);
     }
 }
--- src/Assets/Scripts/Tutorial/TextTimeTrigger.cs
+++ src/Assets/Scripts/Tutorial/TextTimeTrigger.cs
@@ -1,32 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
-using TMPro;
 
-public class TextTrigger : MonoBehaviour
+public class TextTimeTrigger : MonoBehaviour
 {
+
     public TextMeshProUGUI displayText; // Textコンポーネントを使用する
+    [SerializeField] private float displayDelay = 5.0f;
     [SerializeField] private float Time = 3.0f;//デフォルトは3秒
-    [SerializeField] private string Tag = "Player";
     [SerializeField] private GameObject HideObj = null;
 
 
     private void Start()
     {
         displayText.enabled = false; // 開始時にテキストを非表示にする
-    }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag(Tag))
-        {
-            ShowText(); // プレイヤーがエリアに入っており、テキストがまだ表示されていない場合は表示する
-            Invoke("HideText", Time); // Time秒後に非表示にする
-        }
+        Invoke("ShowText", displayDelay);
     }
 
     private void ShowText()
     {
         displayText.enabled = true; // テキストを表示する
+        Invoke("HideText", Time);
     }
 
     private void HideText()
