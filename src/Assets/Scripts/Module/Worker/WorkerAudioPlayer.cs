1fc1afee862527063857bbc6af73372cd45abcdb
HarumaroJP <haruto09.game@gmail.com>
2023-12-05 17:06:12 +0900

    ワーカーのオーディオプレイヤーの追加

---
+++ src/Assets/Scripts/Module/Worker/WorkerAudioPlayer.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using Wanna.DebugEx;
+
+namespace Module.Working
+{
+    [RequireComponent(typeof(AudioSource))]
+    [DisallowMultipleComponent]
+    public class WorkerAudioPlayer : MonoBehaviour
+    {
+        private AudioSource audioSource;
+
+        private void Start()
+        {
+            audioSource = GetComponent<AudioSource>();
+
+            if (audioSource == null)
+            {
+                DebugEx.LogError($"{gameObject.name}にAudioSourceがアタッチされていません");
+            }
+        }
+
+        public void Play()
+        {
+            //鳴らす
+            //audioSource.PlayOneShot();
+        }
+
+        public void SetVolume(float volume)
+        {
+            audioSource.volume = volume;
+        }
+    }
+}
