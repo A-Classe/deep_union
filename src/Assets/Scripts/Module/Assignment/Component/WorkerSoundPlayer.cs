534d4a20c3cd4ff58cf2dc25e5495f289992ca2f
HarumaroJP <haruto09.game@gmail.com>
2023-12-12 23:43:33 +0900

    :bug: ワーカーの音をずらして再生するように修正

--- src/Assets/Scripts/Module/Assignment/Component/WorkerSoundPlayer.cs
+++ src/Assets/Scripts/Module/Assignment/Component/WorkerSoundPlayer.cs
@@ -1,5 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
 using UnityEngine;
 using Wanna.DebugEx;
 
@@ -12,6 +13,9 @@ namespace Module.Working
     {
         private AudioSource audioSource;
         [SerializeField] private AudioClip assignSound;
+        [SerializeField] private float minPlayInterval;
+        [SerializeField] private float maxQueueCount;
+        private int scheduledCount;
 
         private void Start()
         {
@@ -21,12 +25,42 @@ namespace Module.Working
             {
                 DebugEx.LogError($"{gameObject.name}にAudioSourceがアタッチされていません");
             }
+
+            PlayLoop().Forget();
+        }
+
+        private async UniTaskVoid PlayLoop()
+        {
+            CancellationToken canceller = this.GetCancellationTokenOnDestroy();
+            
+            while (!canceller.IsCancellationRequested)
+            {
+                //なにかスケジュールされるまで待機
+                await UniTask.WaitUntil(() => scheduledCount > 0, cancellationToken: canceller);
+
+                audioSource.PlayOneShot(assignSound);
+                scheduledCount--;
+
+                //次の再生まで待機
+                await UniTask.Delay(TimeSpan.FromSeconds(minPlayInterval), cancellationToken: canceller);
+            }
         }
 
         public void Play()
         {
-            //鳴らす
-            audioSource.PlayOneShot(assignSound);
+            //最大待機量を超えたら再生しない
+            if (scheduledCount >= maxQueueCount)
+                return;
+            
+            //再生中じゃないときはそのまま再生
+            if (!audioSource.isPlaying)
+            {
+                audioSource.PlayOneShot(assignSound);
+                return;
+            }
+
+            //スケジュールする
+            scheduledCount++;
         }
     }
 }
