2f434df1a0f6e0d1210ccd6fc1f34fe2afd7b85d
HarumaroJP <haruto09.game@gmail.com>
2023-12-19 22:37:00 +0900

    wip

--- src/Assets/Scripts/Module/Player/State/AutoState.cs
+++ src/Assets/Scripts/Module/Player/State/AutoState.cs
@@ -1,5 +1,7 @@
 using System;
 using UnityEngine;
+using Wanna.DebugEx;
+using Debug = System.Diagnostics.Debug;
 
 namespace Module.Player.State
 {
@@ -10,6 +12,11 @@ namespace Module.Player.State
         public float MaxSpeed;
         public float Acceleralation;
         public float Resistance;
+
+        [Header("回避設定")] public float DetectOffset = 1f;
+        public float DetectDistance;
+        public float AvoidRotation;
+        public LayerMask DetectLayerMask;
     }
 
     /// <summary>
@@ -43,11 +50,28 @@ namespace Module.Player.State
         {
             //徐々に加速
             currentSpeed += movementSetting.Acceleralation * Time.fixedDeltaTime;
-            
+
             //速度制限
             currentSpeed = Mathf.Clamp(currentSpeed, movementSetting.MinSpeed, movementSetting.MaxSpeed);
 
             rigidbody.velocity = rigidbody.transform.forward * currentSpeed;
+
+            PerformAvoidance();
+        }
+
+        private void PerformAvoidance()
+        {
+            Vector3 position = rigidbody.position + new Vector3(0f, movementSetting.DetectOffset, 0f);
+            Vector3 forward = rigidbody.transform.forward;
+
+            DebugEx.DrawLine(position, position + forward * movementSetting.DetectDistance, Color.red, 0.5f);
+
+            if (Physics.Raycast(position, forward, out RaycastHit hitInfo, movementSetting.DetectDistance, movementSetting.DetectLayerMask.value))
+            {
+                float t = -(Mathf.Sign(hitInfo.point.x) * (1 - Mathf.Abs(hitInfo.point.x)));
+                DebugEx.Log(Vector3.up * (movementSetting.AvoidRotation * t));
+                rigidbody.angularVelocity = Vector3.up * (movementSetting.AvoidRotation * t);
+            }
         }
     }
 }
